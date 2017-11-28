b94c3c36d2c11132c84af78a84e3aadd115a9d03
Gregoire Brechenmacher <greg.brech@hotmail.fr>
2017-11-29 00:10:00 +0100

    Form work

--- src/AcceptForm.js
+++ src/AcceptForm.js
@@ -24,45 +24,54 @@ class AcceptForm extends React.Component {
             <Row id="acceptForm" style={style}>
                 <Form>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Nom</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="nom" sm={4}>Nom</Label>
+                        <Col sm={8}>
+                            <Input type="text" name="nom" id="nom" placeholder="Nom" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Prénom</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="prenom" sm={4}>Prénom</Label>
+                        <Col sm={8}>
+                            <Input type="text" name="prenom" id="prenom" placeholder="Prénom" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Profession</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="profession" sm={4}>Profession</Label>
+                        <Col sm={8}>
+                            <Input type="text" name="profession" id="profession" placeholder="Profession" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Langue</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="langue" sm={4}>Langue</Label>
+                        <Col sm={8}>
+                            <Input type="text" name="langue" id="langue" placeholder="Langue" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Situation familiale</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="situationFamiliale" sm={4}>Situation familiale</Label>
+                        <Col sm={8}>
+                            <Input type="text" name="situationFamiliale" id="situationFamiliale" placeholder="Situation Familiale" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Adresse email</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="email" sm={4}>Adresse email</Label>
+                        <Col sm={8}>
+                            <Input type="email" name="email" id="email" placeholder="Adresse email" />
                         </Col>
                     </FormGroup>
                     <FormGroup row>
-                        <Label for="exampleEmail" sm={2}>Téléphone</Label>
-                        <Col sm={10}>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+                        <Label for="telephone" sm={4}>Téléphone</Label>
+                        <Col sm={1}>
+                            <i className="fa fa-phone fa-lg" aria-hidden="true" id="infoPhone" />
+                        </Col>
+                        <Col sm={5}>
+                            <Input type="text" name="telephone" id="telephone" placeholder="Téléphone" />
+                        </Col>
+                        <Col sm={2}>
+                            <Input type="select" name="selectMulti" id="exampleSelectMulti">
+                                <option>Portable</option>
+                                <option>Fixe</option>
+                            </Input>
                         </Col>
                     </FormGroup>
                 </Form>
